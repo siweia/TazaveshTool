1c39d8e45ea7e8187d4b69e8508de290499b5cda
siweia <siwei1990@outlook.com>
2022-04-11 01:31:28 +0800

    Add Runes Notification

---
+++ TazaveshTool/RunesNotification.lua
@@ -0,0 +1,170 @@
+-- 348450, 1323035 蓝
+-- 348451, 1323037 紫
+-- 348447, 1323038 黄
+-- 348437, 1323039 橙
+local texes = {1323035, 1323037, 1323038, 1323039}
+local direcs = {"↖", "↗", "↙", "↘"}
+local colors = {"蓝", "紫", "黄", "橙"}
+local orders = {1, 2, 4, 3}
+local iconString = "|T%s:16:16:0:0:64:64:5:59:5:59|t %s"
+local ticks = 0
+
+local triggerSpell = 346427 -- 触发法术
+local width = 200
+local spacing, iconSpacing = 30, 1
+local slotWidth = 60
+local iconWidth = (slotWidth-iconSpacing)/2
+
+local f = CreateFrame("Frame", "NDuiTazaConsole", UIParent)
+f:SetSize(width, width)
+f:SetPoint("RIGHT", -300, 0)
+f:SetMovable(true)
+f:SetUserPlaced(true)
+f:SetClampedToScreen(true)
+f:EnableMouse(true)
+f:RegisterForDrag("LeftButton")
+f:SetScript("OnDragStart", function() f:StartMoving() end)
+f:SetScript("OnDragStop", function() f:StopMovingOrSizing() end)
+
+local tex = f:CreateTexture(nil, "BACKGROUND")
+tex:SetAllPoints()
+tex:SetTexture("Interface\\Addons\\TazaveshTool\\Media\\gambit")
+f:Hide()
+
+local function onEnter(bu)
+	bu.text:SetTextColor(1, .8, 0)
+end
+
+local function onLeave(bu)
+	bu.text:SetTextColor(1, 1, 1)
+end
+
+local function CreateButton(parent, width, height, text, fontSize)
+	local bu = CreateFrame("Button", nil, parent)
+	bu:SetSize(width, height)
+	if type(text) == "boolean" then
+		local tex = bu:CreateTexture()
+		tex:SetAllPoints()
+		tex:SetTexCoord(.08, .92, .08, .92)
+		tex:SetTexture(fontSize)
+		local hl = bu:CreateTexture(nil, "HIGHLIGHT")
+		hl:SetAllPoints()
+		hl:SetColorTexture(1, 1, 1, .25)
+		bu.tex = tex
+	else
+		local fs = bu:CreateFontString()
+		fs:SetPoint("CENTER")
+		fs:SetFontObject(Game16Font)
+		fs:SetText(text)
+		bu.text = fs
+		bu:SetScript("OnEnter", onEnter)
+		bu:SetScript("OnLeave", onLeave)
+	end
+	return bu
+end
+
+local function showTooltip(icon)
+	GameTooltip:SetOwner(icon, "ANCHOR_TOP")
+	GameTooltip:ClearLines()
+	GameTooltip:AddLine(format(iconString, texes[icon.id], colors[icon.id]))
+	GameTooltip:Show()
+end
+
+local slots = {}
+
+local function clickIcon(self)
+	for i = 1, 4 do
+		slots[i].icons[self.id].tex:SetAlpha(i ~= self.index and .2 or 1)
+		slots[i].icons[self.id].tex:SetDesaturated(i ~= self.index)
+	end
+	for j = 1, 4 do
+		slots[self.index].icons[j].tex:SetAlpha(j ~= self.id and .2 or 1)
+		slots[self.index].icons[j].tex:SetDesaturated(j ~= self.id)
+	end
+	ticks = ticks + 1
+end
+
+local anchor = {
+	[1] = {"TOPLEFT", 5, -5},
+	[2] = {"TOPRIGHT", -5, -5},
+	[3] = {"BOTTOMLEFT", 5, 5},
+	[4] = {"BOTTOMRIGHT", -5, 5},
+}
+for i = 1, 4 do
+	local slot = CreateFrame("Frame", nil, f)
+	slot:SetSize(slotWidth, slotWidth)
+	slot:SetPoint(unpack(anchor[i]))
+	slots[i] = slot
+
+	slot.icons = {}
+	for j = 1, 4 do
+		local icon = CreateButton(slot, iconWidth, iconWidth, true, texes[j])
+		icon:SetPoint("TOPLEFT", (j-1)%2 * (iconWidth+iconSpacing), - (j>2 and (iconWidth+iconSpacing) or 0))
+		icon.index = i
+		icon.id = j
+		slot.icons[j] = icon
+		icon:SetScript("OnClick", clickIcon)
+		icon:SetScript("OnEnter", showTooltip)
+		icon:SetScript("OnLeave", GameTooltip_Hide)
+	end
+end
+
+local reset = CreateButton(f, slotWidth, 22, RESET, 16)
+reset:SetPoint("TOPLEFT", f, "BOTTOMLEFT", 10, 0)
+reset:SetScript("OnClick", function()
+	for i = 1, 4 do
+		for j = 1, 4 do
+			slots[i].icons[j].tex:SetAlpha(1)
+			slots[i].icons[j].tex:SetDesaturated(false)
+		end
+	end
+	ticks = 0
+end)
+
+local send = CreateButton(f, slotWidth, 22, SEND_LABEL, 16)
+send:SetPoint("TOPRIGHT", f, "BOTTOMRIGHT", -10, 0)
+send:SetScript("OnClick", function()
+	if ticks < 3 then return end
+
+	local text = ""
+	for _, i in pairs(orders) do
+		for j = 1, 4 do
+			local icon = slots[i].icons[j].tex
+			if icon:GetAlpha() == 1 then
+				text = text..direcs[i]..colors[j].."   "
+				break
+			end
+		end
+	end
+	SendChatMessage(text, IsPartyLFG() and "INSTANCE_CHAT" or IsInRaid() and "RAID" or "PARTY")
+end)
+
+local eventFrame = CreateFrame("Frame")
+eventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+eventFrame:SetScript("OnEvent", function(_, event, arg1)
+	if event == "PLAYER_ENTERING_WORLD" then
+		local instID = select(8, GetInstanceInfo())
+		if instID == 2441 then
+			eventFrame:RegisterEvent("ENCOUNTER_START")
+		else
+			eventFrame:UnregisterEvent("ENCOUNTER_START")
+		end
+	elseif event == "ENCOUNTER_START" then
+		if arg1 == 2426 then -- 希尔布兰德
+			eventFrame:RegisterUnitEvent("UNIT_AURA", "player")
+		else
+			eventFrame:UnregisterEvent("UNIT_AURA")
+		end
+	elseif event == "UNIT_AURA" then
+		f:Hide()
+
+		for i = 1, 40 do
+			local name, _, _, _, _, _, _, _, _, spellID = UnitBuff("player", i)
+			if not name then break end
+			if spellID == triggerSpell then
+				f:Show()
+				return
+			end
+		end
+	end
+end)
